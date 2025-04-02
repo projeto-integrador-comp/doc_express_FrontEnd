ac9cd5f4ee3829bada14b2f4e8dc077c1903093d
Miguel Lúcio <miguelluciosilva@gmail.com>
2025-04-02 15:11:35 -0300

    feat: routes main

---
+++ src/routes/index.jsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import { LoginPage } from "../pages/LoginPage/indes";
+import { RegisterPage } from "../pages/RegisterPage";
+import { DashboardPage } from "../pages/DashboardPage";
+import { ErrorPage } from "../pages/ErrorPage";
+
+export const RoutesMain = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="*" element={<ErrorPage />} />
+    </Routes>
+  );
+};
