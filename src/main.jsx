98ba36bc9bac2fbf2f8c0e80d95e60c9e9841a98
Miguel Lúcio <miguelluciosilva@gmail.com>
2025-04-03 13:03:03 -0300

    feat: global styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import "./styles/index.scss"
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
