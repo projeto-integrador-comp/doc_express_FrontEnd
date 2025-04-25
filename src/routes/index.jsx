c943e02c7567c6e4a54e6467ef92ff0ffda689fa
Miguel Lúcio <miguelluciosilva@gmail.com>
2025-04-25 20:47:19 -0300

    feat: route protection

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,14 +3,19 @@ import { LoginPage } from "../pages/LoginPage";
 import { RegisterPage } from "../pages/RegisterPage";
 import { DashboardPage } from "../pages/DashboardPage";
 import { ErrorPage } from "../pages/ErrorPage";
-
+import { PublicRoutes } from "./PublicRoutes";
+import { PrivateRoutes } from "./PrivateRoutes";
 
 export const RoutesMain = () => {
   return (
     <Routes>
+      {/* <Route element={<PublicRoutes />}> */}
       <Route path="/" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
+      {/* </Route> */}
+      {/* <Route element={<PrivateRoutes />}> */}
       <Route path="/dashboard" element={<DashboardPage />} />
+      {/* </Route> */}
       <Route path="*" element={<ErrorPage />} />
     </Routes>
   );
