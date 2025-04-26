d84790717d52730bff7118b93891244636f490e0
Miguel Lúcio <miguelluciosilva@gmail.com>
2025-04-26 12:59:25 -0300

    feat: update document form

---
+++ src/components/forms/UpdateDocumentForm/validator.js
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const schema = z.object({
+  submissionDate: z.string().regex(/^\d{4}-\d{2}-\d{2}$/, "Selecione uma data"),
+  documentName: z
+    .string()
+    .max(50, "Máximo 50 caracteres")
+    .min(2, "Mínimo 2 caracteres")
+    .nonempty("O nome é obrigatório"),
+  note: z.string().max(50, "Máximo 50 caracteres").default(""),
+  delivered: z.boolean().default(false),
+});
