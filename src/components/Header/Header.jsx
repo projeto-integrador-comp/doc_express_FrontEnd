845142c1ff49196a50346c2169513ac22b6871a7
rubenslaurindo <rubens.anderson@gmail.com>
2025-04-28 21:57:17 -0300

    Botão sanduiche no botão cadastras documentos

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,33 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import styles from "./Header.module.scss";
 import { DocumentContext } from "../../providers/DocumentContext";
 
 const Header = () => {
   const { setHiddenCreateDocument } = useContext(DocumentContext);
+  const [isMobile, setIsMobile] = useState(false);
+
+  // Função para detectar se a tela é pequena
+  const handleResize = () => {
+    setIsMobile(window.innerWidth <= 768);
+  };
+
+  // Verifica quando a página carrega e quando a tela é redimensionada
+  useEffect(() => {
+    handleResize(); // Checa na primeira vez
+    window.addEventListener("resize", handleResize); // Atualiza se mudar o tamanho
+    return () => window.removeEventListener("resize", handleResize); // Limpa evento
+  }, []);
+
   return (
     <header className={styles.header}>
       <div className={styles.headerContainer}>
         <h1 className={styles.logo}>DocExpress</h1>
+
         <button
           className={styles.addButton}
           onClick={() => setHiddenCreateDocument(false)}
         >
-          Cadastrar Documento
+          {isMobile ? "☰" : "Cadastrar Documento"}
         </button>
       </div>
     </header>
